09eb79593da730a09bd43916d48d4b805e5d56ab
luckwchen <luckwchen@gmail.com>
2013-08-27 00:48:25 +0000

    更新bug

--- tool.js
+++ tool.js
@@ -9,100 +9,104 @@ var BRQ_NAME='brqTr';
 
 //过滤sp占整个Sp和值的百分点
 function flushSpPointGame(flag,minSpPointNum,minSumSpNum,minSpNum){
-	var gamePlayArray=[];
-	var elName=flag ? RQ_NAME:BRQ_NAME;
-	var trList=document.getElementsByName(elName);
+   var gamePlayArray=[];
+   var elName=flag ? RQ_NAME:BRQ_NAME;
+   var trList=document.getElementsByName(elName);
 
-	for(var i=0;i<trList.length;i++){
-		var spanList=trList[i].getElementsByTagName("span");
-		var sumSpNum=0;
-		var stop=false;
-		for(var j=0;j<spanList.length;j++){
-			var spNum=parseFloat(spanList[j].innerText);
-			if(spNum<minSpNum){
-				//过滤最小Sp值
-				$(trList[i].parentElement).hide();
-				stop=true;
-				break;
-			}
-			sumSpNum+=spNum;
-		}
-		if(stop){
-			continue;
-		}
-		
-		if(sumSpNum!=null){
-			//过滤Sp最小和值
-			if(sumSpNum<minSumSpNum){
-				$(trList[i].parentElement).hide();
-				continue;
-			}
-		}
-		for(var j=0;j<spanList.length;j++){
-			var spNum=parseFloat(spanList[j].innerText);
-			var point=spNum/sumSpNum;
-			//过滤最小SP百分点
-			if(point<minSpPointNum){
-				$(trList[i].parentElement).hide();
-				break;
-			}
-		}
-		gamePlayArray.push(trList[i].parentElement.id);
-		$(trList[i].parentElement).show();
+   for(var i=0;i<trList.length;i++){
+      var spanList=trList[i].getElementsByTagName("span");
+      var sumSpNum=0;
+      var stop=false;
+      for(var j=0;j<spanList.length;j++){
+         var spNum=parseFloat(spanList[j].innerText);
+         if(spNum<minSpNum){
+            //过滤最小Sp值
+            $(trList[i].parentElement).hide();
+            stop=true;
+            break;
+         }
+         sumSpNum+=spNum;
+      }
+      if(stop){
+         continue;
+      }
+      
+      if(sumSpNum!=null){
+         //过滤Sp最小和值
+         if(sumSpNum<minSumSpNum){
+            $(trList[i].parentElement).hide();
+            continue;
+         }
+      }
+      for(var j=0;j<spanList.length;j++){
+         var spNum=parseFloat(spanList[j].innerText);
+         var point=spNum/sumSpNum;
+         //过滤最小SP百分点
+         if(point<minSpPointNum){
+            $(trList[i].parentElement).hide();
+        stop=true;
+            break;
+         }
+      }
+    if(stop){
+         continue;
+      }
+      gamePlayArray.push(trList[i].parentElement.id);
+      $(trList[i].parentElement).show();
 
-	}
+   }
 
-	makeVoteStr(gamePlayArray);
+   makeVoteStr(gamePlayArray);
 }
 
 function makeVoteStr(gameList){
-	//生成投注文件
-	var numArray=[0,1,3];
-	var content='';
-	while(true){
-		if(gameList.length<2){
-			console.debug(content);
-			break;
-		}
-		console.debug(gameList);
-		var game1=gameList[0];
-		gameList.splice(0,1);
-		var game2Index=Math.round(Math.random()*99999999)%gameList.length;
-		console.debug(game2Index);
-		var game2=gameList[game2Index];
-		gameList.splice(game2Index,1);
+   //生成投注文件
+   var numArray=[0,1,3];
+   var content='';
+   while(true){
+      if(gameList.length<2){
+         console.debug(content);
+         break;
+      }
+      console.debug(gameList);
+      var game1=gameList[0];
+      gameList.splice(0,1);
+      var game2Index=Math.round(Math.random()*99999999)%gameList.length;
+      console.debug(game2Index);
+      var game2=gameList[game2Index];
+      gameList.splice(game2Index,1);
 
-		//
-		for(var i=0;i<numArray.length;i++){
-			
-			var gameDateStr=game1.substr(0,8);
-			var game1Date=new Date();
-			game1Date.setYear(gameDateStr.substr(0,4));
-			game1Date.setMonth(parseInt(gameDateStr.substr(4,2))-1);
-			game1Date.setDate(gameDateStr.substr(6,2));
-			var game1Day=game1Date.getDay();
-			game1Day=game1Day==0 ? 7:game1Day;
-			var playNum=game1.substr(8);
+      //
+      for(var i=0;i<numArray.length;i++){
+         
+         var gameDateStr=game1.substr(0,8);
+         var game1Date=new Date();
+         game1Date.setYear(gameDateStr.substr(0,4));
+         game1Date.setMonth(parseInt(gameDateStr.substr(4,2))-1);
+         game1Date.setDate(gameDateStr.substr(6,2));
+         var game1Day=game1Date.getDay();
+         game1Day=game1Day==0 ? 7:game1Day;
+         var playNum=game1.substr(8);
 
-			for(var j=0;j<numArray.length;j++){
-				console.debug(game2)
-				var game2DateStr=game2.substr(0,8);
-				var game2Date=new Date();
-				game2Date.setYear(game2DateStr.substr(0,4));
-				game2Date.setMonth(parseInt(game2DateStr.substr(4,2))-1);
-				game2Date.setDate(game2DateStr.substr(6,2));
-				var game2Day=game2Date.getDay();
-				game2Day=game2Day==0 ? 7:game2Day;
-				var playNum2=game2.substr(8);
+         for(var j=0;j<numArray.length;j++){
+            console.debug(game2)
+            var game2DateStr=game2.substr(0,8);
+            var game2Date=new Date();
+            game2Date.setYear(game2DateStr.substr(0,4));
+            game2Date.setMonth(parseInt(game2DateStr.substr(4,2))-1);
+            game2Date.setDate(game2DateStr.substr(6,2));
+            var game2Day=game2Date.getDay();
+            game2Day=game2Day==0 ? 7:game2Day;
+            var playNum2=game2.substr(8);
 
-				content+=game1Day+playNum+':['+numArray[i]+']/'+game2Day+playNum2+':['+numArray[j]+']\n';
+            content+=game1Day+playNum+':['+numArray[i]+']/'+game2Day+playNum2+':['+numArray[j]+']\n';
 
-			}
+         }
 
-		}
+      }
 
-	}
-	
+   }
+   
 }
 
 
