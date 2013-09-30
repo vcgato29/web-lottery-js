0b074b49b89725eb8491f9b4deb4d0f6d82ac629
luckwchen <luckwchen@gmail.com>
2013-09-30 14:05:04 +0000

    update

--- tool.js
+++ tool.js
@@ -43,9 +43,9 @@ function flushSpPointGame(flag,minSpPointNum,minSumSpNum,minSpNum,secondSpMinNum
       
       var rqnum= $("#"+gameId+" td[name='rq_td'] strong").html();
       if(rqnum!=null){
+        console.debug(rqnum);
         rqnum=rqnum.replace('+','');
         if(parseInt(rqnum)>maxRq){
-          console.debug(rqnum);
           continue;
         }
       }
